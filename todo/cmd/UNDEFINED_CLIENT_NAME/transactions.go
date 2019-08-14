0b3dc5259d102f021cee1e2f83ed06961ef500eb
dylanverstraete <dylan_verstraete1@hotmail.com>
2019-08-14 13:31:08 +0200

    adds client transactions configuration

--- todo/cmd/UNDEFINED_CLIENT_NAME/transactions.go
+++ todo/cmd/UNDEFINED_CLIENT_NAME/transactions.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"github.com/threefoldtech/rivine/extensions/minting"
 	"github.com/threefoldtech/rivine/pkg/client"
+	"github.com/threefoldtech/rivine/types"
 )
 
 // RegisterStandardTransactions registers the goldchain-specific transactions as required for the standard network.
 func RegisterStandardTransactions(cli *client.CommandLineClient) {
-	// TODO: register custom transaction controllers for CLI if required
+	registerTransactions(cli)
 }
 
 // RegisterTestnetTransactions registers the goldchain-specific transactions as required for the test network.
 func RegisterTestnetTransactions(cli *client.CommandLineClient) {
-	// TODO: register custom transaction controllers for CLI if required
+	registerTransactions(cli)
 }
 
 // RegisterDevnetTransactions registers the goldchain-specific transactions as required for the dev network.
 func RegisterDevnetTransactions(cli *client.CommandLineClient) {
-	// TODO: register custom transaction controllers for CLI if required
+	registerTransactions(cli)
+}
+
+func registerTransactions(cli *client.CommandLineClient) {
+	// create minting plugin client...
+	mintingCLI := mintingcli.NewPluginConsensusClient(cli)
+	// ...and register minting types
+	types.RegisterTransactionVersion(gctypes.MinterDefinitionTxVersion, minting.MinterDefinitionTransactionController{
+		MintConditionGetter: mintingCLI,
+		TransactionVersion:  gctypes.MinterDefinitionTxVersion,
+	})
+	types.RegisterTransactionVersion(gctypes.CoinCreationTxVersion, minting.CoinCreationTransactionController{
+		MintConditionGetter: mintingCLI,
+		TransactionVersion:  gctypes.CoinCreationTxVersion,
+	})
+	types.RegisterTransactionVersion(gctypes.CoinDestructionTxVersion, minting.CoinDestructionTransactionController{
+		TransactionVersion: gctypes.CoinDestructionTxVersion,
+	})
 }
